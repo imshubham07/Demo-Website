9eb3abfedc7b22afa4d8c62b5dd07dbe8a79189a
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-19 15:14:22 +0530

    Demo Website in React

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -72,8 +72,8 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        <a href="https://github.com/imshubham07" className="hover:underline">
+                            imshubham07
                         </a>
                         . All Rights Reserved.
                     </span>
